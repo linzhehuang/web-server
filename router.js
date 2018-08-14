5eb5c4892955d42ca9e299a7e18cf8b8edd62764
linzhehuang <linzhehuang@outlook.com>
2018-08-14 17:59:25 +0800

    charpter:add router feature - part two

--- router.js
+++ router.js
@@ -1,4 +1,38 @@
 const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+function getErrorInfo(errorType) {
+  // 错误信息列表
+   const errorInfo = {
+     'NOT_FOUND' : ['404', 'Not Found'],
+     'FORBIDDEN' : ['403', 'Forbidden']
+   }
+   return errorInfo[errorType];
+}
+
+function writeErrorPage(response, errorType) {
+  // 错误页面模板
+  const errorPageTemplate = `
+    <html>
+      <head>
+        <title>{{ errorText }}</title>
+      </head>
+      <body>
+        <h1>Error: {{ errorCode }} - {{ errorText }}</h1>
+      </body>
+    </html>
+  `;
+  // 获取错误码和错误文字提示
+  var [errorCode, errorText] = getErrorInfo(errorType);
+  response.writeHeader(parseInt(errorCode), {
+    "Content-Type" : "text/html"
+  });
+  response.end(errorPageTemplate
+    .replace(/{{ errorText }}/g, errorText)
+    .replace(/{{ errorCode }}/g, errorCode)
+  );
+}
 
 exports.register = function(request, response, mapping) {
   // 解析请求路径
@@ -10,18 +44,27 @@ exports.register = function(request, response, mapping) {
       return;
     }
   }
-  // 请求路径不存在返回404页面
-  response.writeHeader(404, {
-    "Content-Type" : "text/html"
+  // 请求路径为文件返回文件内容
+  var file = path.resolve(__dirname, '.' + pathName);
+  fs.exists(file, function(exists) {
+    // 请求路径不存在返回404页面
+    if(!exists) {
+      writeErrorPage(response, 'NOT_FOUND');
+    }
+    else {
+      var stat = fs.statSync(file);
+      // 请求路径为目录返回403页面
+      if(stat.isDirectory()) {
+        writeErrorPage(response, 'FORBIDDEN');
+      }
+      else {
+        response.writeHeader(200, {
+          "Content-Type" : "text/html"
+        });
+        response.end(
+          fs.readFileSync(file, 'utf-8')
+        );
+      }
+    }
   });
-  response.end(`
-    <html>
-      <head>
-        <title>NOT FOUND</title>
-      </head>
-      <body>
-        <h1>404 NOT FOUND</h1>
-      </body>
-    </html>
-  `);
 }
